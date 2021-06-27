1d7fe30019f93b160d63d9af335b3e5853490f55
valhein <valhein256@gmail.com>
2021-06-27 14:25:06 +0800

    Update

--- tree.go
+++ tree.go
@@ -6,18 +6,6 @@ type Node struct {
 	right *Node
 }
 
-type Tree struct {
-	node *Node
-}
-
-func (t *Tree) insert(value int) {
-	if t.node == nil {
-		t.node = &Node{value: value}
-	} else {
-		t.node.insert(value)
-	}
-}
-
 func (n *Node) insert(value int) {
 	if value <= n.value {
 		if n.left == nil {
@@ -34,6 +22,34 @@ func (n *Node) insert(value int) {
 	}
 }
 
+type Tree struct {
+	node *Node
+}
+
+func (t *Tree) insert(value int) {
+	if t.node == nil {
+		t.node = &Node{value: value}
+	} else {
+		t.node.insert(value)
+	}
+}
+
+func (n *Node) search(value int) int {
+	if n == nil {
+		return -1
+	}
+
+	if value == n.value {
+		return n.value
+	}
+
+	if value < n.value {
+		return n.left.search(value)
+	} else {
+		return n.right.search(value)
+	}
+}
+
 func printTree(n *Node) {
 	if n == nil {
 		return
@@ -47,7 +63,13 @@ func main() {
 	t := &Tree{}
 	t.insert(10)
 	t.insert(3)
+	t.insert(2)
+	t.insert(1)
+	t.insert(3)
 	t.insert(7)
 	t.insert(23)
 	printTree(t.node)
+
+	println(t.node.search(10))
+	println(t.node.search(12))
 }
